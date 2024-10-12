8c3c1ed29ebcc45956f6ff5ba8ae4104e87a9069
Bibhushan Karki <bibhushan.karki11@gmail.com>
2024-10-12 17:18:07 +0545

    playing around with the files and folders

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Greet from "../components/Greet";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+      <Greet />
+    </View>
+  );
+};
+
+export default Index;
